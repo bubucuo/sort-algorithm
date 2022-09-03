7184c1df8d6442a3c91486881c85372a0e185760
bubucuo <276185876@qq.com>
2022-09-03 17:29:08 +0800

    save ArrayTimSortImpl

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const {ArrayTimSortImpl} = require("./timSort");
 
-exports.TimSort = (array, compare) => {
+exports.TimSort = (array, compare = (a, b) => a - b) => {
   const sortState = {
     workArray: array,
     Compare: compare,
-    tempArray: [],
-    pendingRunsSize: 0,
-    pendingRuns: [],
+    // tempArray: [],
+    // pendingRunsSize: 0,
+    // pendingRuns: [],
 
-    minGallop: 7,
+    // minGallop: 7,
   };
 
   ArrayTimSortImpl(sortState);
 
+  // 返回有序数组
   return sortState.workArray;
 };
