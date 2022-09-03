5f98550871be5734e3ad6f88843396c86cda28fb
bubucuo <276185876@qq.com>
2022-09-03 18:24:09 +0800

    save 1. 从左向右遍历数组，获取分区（run）

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -1,50 +1,64 @@
 describe("数组Diff", () => {
   const {TimSort} = require("../index");
   const sort = TimSort;
-  it("整数12345", () => {
+
+  it("分区12345，不考虑分区的真实长度", () => {
     const arr = [5, 1, 2, 4, 3];
 
-    const compare = (a, b) => a - b;
-
-    const res = sort(arr, compare);
-    expect(res).toBe(arr.sort());
-  });
-
-  it("整数64", () => {
-    const arr = [];
-
-    for (let i = 0; i < 64; i++) {
-      arr[i] = (Math.random() * 1000) >> 1;
-    }
+    // 1 5
+    // 2 4
+    // 3
 
     const compare = (a, b) => a - b;
 
     const res = sort(arr, compare);
-    expect(res).toBe(arr.sort());
-  });
-  it("整数164", () => {
-    const arr = [];
-
-    for (let i = 0; i < 164; i++) {
-      arr[i] = (Math.random() * 1000) >> 1;
-    }
-
-    const compare = (a, b) => a - b;
-
-    const res = sort(arr, compare);
-    expect(res).toBe(arr.sort());
+    expect(res).toEqual([1, 5, 2, 4, 3]);
   });
 
-  it("小数164", () => {
-    const arr = [];
+  // it("整数12345", () => {
+  //   const arr = [5, 1, 2, 4, 3];
 
-    for (let i = 0; i < 164; i++) {
-      arr[i] = Math.random() * 1000;
-    }
+  //   const compare = (a, b) => a - b;
 
-    const compare = (a, b) => a - b;
+  //   const res = sort(arr, compare);
+  //   expect(res).toBe(arr.sort());
+  // });
 
-    const res = sort(arr, compare);
-    expect(res).toBe(arr.sort());
-  });
+  // it("整数64", () => {
+  //   const arr = [];
+
+  //   for (let i = 0; i < 64; i++) {
+  //     arr[i] = (Math.random() * 1000) >> 1;
+  //   }
+
+  //   const compare = (a, b) => a - b;
+
+  //   const res = sort(arr, compare);
+  //   expect(res).toBe(arr.sort());
+  // });
+  // it("整数164", () => {
+  //   const arr = [];
+
+  //   for (let i = 0; i < 164; i++) {
+  //     arr[i] = (Math.random() * 1000) >> 1;
+  //   }
+
+  //   const compare = (a, b) => a - b;
+
+  //   const res = sort(arr, compare);
+  //   expect(res).toBe(arr.sort());
+  // });
+
+  // it("小数164", () => {
+  //   const arr = [];
+
+  //   for (let i = 0; i < 164; i++) {
+  //     arr[i] = Math.random() * 1000;
+  //   }
+
+  //   const compare = (a, b) => a - b;
+
+  //   const res = sort(arr, compare);
+  //   expect(res).toBe(arr.sort());
+  // });
 });
--- timSort.js
+++ timSort.js
@@ -11,29 +11,29 @@ function ArrayTimSortImpl(sortState) {
   let remaining = length;
 
   // 计算分区的最小长度值
-  const minRunLength = ComputeMinRunLength(remaining);
+  // const minRunLength = ComputeMinRunLength(remaining);
 
   while (remaining !== 0) {
     // 寻找分区，并返回分区长度值
     let currentRunLength = CountAndMakeRun(sortState, low, low + remaining);
-    if (currentRunLength < minRunLength) {
-      // 扩展分区
-      const forceRunLength = Math.min(minRunLength, remaining);
-      BinaryInsertionSort(
-        sort,
-        low,
-        low + currentRunLength,
-        low + forceRunLength
-      );
+    // if (currentRunLength < minRunLength) {
+    //   // 扩展分区
+    //   const forceRunLength = Math.min(minRunLength, remaining);
+    //   BinaryInsertionSort(
+    //     sort,
+    //     low,
+    //     low + currentRunLength,
+    //     low + forceRunLength
+    //   );
 
-      currentRunLength = forceRunLength;
-    }
+    //   currentRunLength = forceRunLength;
+    // }
 
-    // 分区入栈
-    PushRun(sortState, low, currentRunLength);
+    // // 分区入栈
+    // PushRun(sortState, low, currentRunLength);
 
-    // 合并分区
-    MergeCollapse(sortState);
+    // // 合并分区
+    // MergeCollapse(sortState);
 
     // 寻找下一个分区
     low = low + currentRunLength;
@@ -41,10 +41,66 @@ function ArrayTimSortImpl(sortState) {
   }
 
   // 合并栈中的所有的所有分区， 直到只剩下一个，排序结束
-  MergeForceCollapse(sortState);
+  // MergeForceCollapse(sortState);
 }
 
-function CountAndMakeRun() {}
+// 寻找分区，并返回分区长度值
+function CountAndMakeRun(sortState, lowArg, high) {
+  const low = lowArg + 1;
+  if (low === high) {
+    return 1;
+  }
+
+  let runLength = 2;
+
+  const workArray = sortState.workArray;
+  const elementLow = workArray[low];
+  const elementLowPre = workArray[low - 1];
+  let order = sortState.Compare(elementLow, elementLowPre);
+
+  //根据前两个元素来判断是否是降序
+  const isDescending = order < 0;
+
+  const previousElement = elementLow;
+  for (let idx = low + 1; idx < high; idx++) {
+    const currentElement = workArray[idx];
+    order = sortState.Compare(currentElement, previousElement);
+
+    if (isDescending) {
+      // 严格降序
+      if (order >= 0) {
+        break;
+      }
+    } else {
+      // 升序
+      if (order < 0) {
+        break;
+      }
+    }
+
+    previousElement = currentElement;
+    ++runLength;
+  }
+
+  // 如果得到的是严格降序子序列，翻转就行了
+  if (isDescending) {
+    ReverseRange(workArray, lowArg, lowArg + runLength);
+  }
+  return runLength;
+}
+
+// 根据数组下标翻转数组
+function ReverseRange(array, from, to) {
+  let low = from,
+    high = to - 1;
+  while (low < high) {
+    const elementLow = array[low];
+    const elementHigh = array[high];
+
+    array[low++] = elementHigh;
+    array[high--] = elementLow;
+  }
+}
 
 function ComputeMinRunLength() {}
 
@@ -53,3 +109,5 @@ function BinaryInsertionSort() {}
 function PushRun() {}
 
 function MergeCollapse() {}
+
+exports.ArrayTimSortImpl = ArrayTimSortImpl;
